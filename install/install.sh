61f49822b43d4b558034da3129a47a403e433c4b
aap <aap@papnet.eu>
2026-03-09 19:13:33 +0100

    fix install script to not pull latest submodules; update blincolnlights with sequence break fixes and hsc

--- install/install.sh
+++ install/install.sh
@@ -60,23 +60,7 @@ done
 # pull in git submodules (for pdp1 sim itself, and the p7sim Type 30 display)
 # =============================================================================
 
-while true; do
-    echo
-    read -p "Update git submodules (required for first install)? " yn
-    case $yn in
-        [Yy]* )
-		git submodule init
-		#git submodule update --remote --force
-		git submodule update --init --remote --recursive --force
-		break
-		;;
-        [Nn]* ) 
-            echo Skipped updating submodules from github
-	    break
-            ;;
-        * ) echo "Please answer yes or no.";;
-    esac
-done
+git submodule update --init --recursive
 
 
 # Install required dependencies
